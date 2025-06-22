ad9259c377da4b1fb54290a49917055bf19e7c6e
codedeep9 <saideep.meka123@gmail.com>
2025-06-22 17:47:47 +0530

    footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer";
 import { ROUTES } from "./routes/constants";
 
 const Home = lazy(() => import("./pages/Home"));
@@ -9,15 +10,18 @@ const Home = lazy(() => import("./pages/Home"));
 function App() {
   return (
     <Router>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col min-h-screen">
         <Header className="w-full" />
-        <div className="md:max-w-[80%] max-w-[90%]">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path={ROUTES.HOME} element={<Home />} />
-            </Routes>
-          </Suspense>
-        </div>
+        <main className="flex-1 flex flex-col items-center">
+          <div className="md:max-w-[80%] max-w-[90%] w-full">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path={ROUTES.HOME} element={<Home />} />
+              </Routes>
+            </Suspense>
+          </div>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
